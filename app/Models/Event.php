a7a7c70f72abb24fd19b8bef944f969cb17eb41a
losimen <koval.pavlo03@gmail.com>
2023-05-27 20:30:17 +0300

    event work

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -12,12 +12,7 @@ class Event extends Model
 
     public function volunteers()
     {
-        return $this->belongsToMany(Volunteer::class, 'event_volunteers')
-            ->withPivot('event_id', 'volunteer_id');
-    }
-
-    public function users(): BelongsToMany
-    {
         return $this->belongsToMany(User::class, 'event_volunteers', 'event_id', 'volunteer_id');
+
     }
 }
